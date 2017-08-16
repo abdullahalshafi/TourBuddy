c1393682cad10bc412a6091b268226de88afccab
onirban <onirnab4385@diu.edu.bd>
2017-08-16 12:23:08 +0600

    updated nearby feature

---
+++ app/src/main/java/com/company/appbrkr/tourbuddy/service/RetrofitMaps.java
@@ -0,0 +1,20 @@
+package com.company.appbrkr.tourbuddy.service;
+
+
+import com.company.appbrkr.tourbuddy.pojo.Example;
+
+import retrofit.Call;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
+
+public interface RetrofitMaps {
+
+    /*
+     * Retrofit get annotation with our URL
+     * And our method that will return us details of student.
+     */
+    @GET("api/place/nearbysearch/json?sensor=true&key=AIzaSyDN7RJFmImYAca96elyZlE5s_fhX-MMuhk")
+    Call<Example> getNearbyPlaces(@Query("type") String type, @Query("location") String location, @Query("radius") int radius);
+
+}
